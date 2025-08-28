fd5dc823fe58b32a6d830866689811950088b08f
sandeep kumar singh <singhsandeepkumar008@gmail.com>
2025-08-28 11:37:38 +0000

    fixed

---
+++ src/lib/redis.ts
@@ -0,0 +1,16 @@
+// lib/redis.ts
+import { createClient } from 'redis';
+
+let redisClient: ReturnType<typeof createClient> | null = null;
+
+export const getRedisClient = async () => {
+  if (!redisClient) {
+    redisClient = createClient({
+      url: process.env.REDIS_URL,
+    });
+
+    redisClient.on('error', (err) => console.error('Redis Client Error', err));
+    await redisClient.connect();
+  }
+  return redisClient;
+};
